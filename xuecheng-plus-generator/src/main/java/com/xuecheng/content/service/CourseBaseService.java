4d89d5bb10c0784d0470d2a30b21e01520ccbaf3
RuokeZhang <zhang.12783@osu.edu>
2024-07-06 18:44:11 +0800

    courseBase interface finished

--- xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/CourseBaseService.java
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/CourseBaseService.java
@@ -1,7 +1,10 @@
 package com.xuecheng.content.service;
 
-import com.xuecheng.content.model.po.CourseBase;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
 
 /**
  * <p>
@@ -12,5 +15,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-02-11
  */
 public interface CourseBaseService extends IService<CourseBase> {
-
+    /**
+     * 课程分页查询
+     * @param pageParams 分页查询参数
+     * @param courseParamsDto 查询条件
+     * @return 查询结果
+     */
+    public PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto courseParamsDto);
 }
